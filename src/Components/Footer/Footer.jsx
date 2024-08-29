63754c8d33e7a6dbf57c1edd2dc6c7c2411eef0e
Yogendra Yadav <yogendraydv27@gmail.com>
2024-08-29 11:10:14 +0530

    Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="footer">
         <hr/>
         <div className="footer-bottom">
-            <p className="footer-bottom-left">© Your Portfolio. All rights reserved.</p>
+            <p className="footer-bottom-left">© Personal Portfolio. All rights reserved.</p>
         </div>
         <div className="footer-bottom-right">
             <p>Term of Services</p>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,31 +11,31 @@ const Navbar = () => {
   const [menu, setMenu] = React.useState("home"); 
   const underline = nav_underline;
 
-  {/*const menuRef = React.useRef();
+  const menuRef = React.useRef();
 
   const openMenu = () => {
     menuRef.current.style.right="0";
   }
   const closeMenu = () => {
     menuRef.current.style.right="-350px";
-  }*/}
+  }
 
 
   return (
     <div className='navbar'>
-      {/*<div className='w-full'>*/}
-      {/* <img src={logo} alt=""/> */}
-     {/* <img src={menu_open} alt="" className='nav-mob-open' onClick={openMenu} />*/}
-      <ul className="nav-menu">
-       {/* <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />*/}
+      <div className='w-full'>
+      {/* <img src={logo} alt="" className='nav-logo'/> */}
+     <img src={menu_open} alt="" className='nav-mob-open' onClick={openMenu} />
+      <div ref={menuRef} className="nav-menu">
+       <img src={menu_close} alt="" className="nav-mob-close"  onClick={closeMenu} />
         <li><AnchorLink className='anchor-link' offset={50} href='#home'><p onClick={()=>setMenu("home")}>    Home</p>     </AnchorLink> {menu==="home"?<img src={underline} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>   About Me</p> </AnchorLink> {menu==="about"?<img src={underline} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p> </AnchorLink> {menu==="services"?<img src={underline} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setMenu("work")}>    Portfolio</p></AnchorLink> {menu==="work"?<img src={underline} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}> Contact</p>  </AnchorLink> {menu==="contact"?<img src={underline} alt=''/>:<></>}</li>
-      </ul>
-      {/* <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div> */}
-      {/*</div>*/}
+      </div>
+      <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
+      </div>
     </div>
   )
 }
